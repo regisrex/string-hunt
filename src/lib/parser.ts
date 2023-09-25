227b55832db2f124b78439c603c1e54c1dec31df
pacifiquem <Murangwa Pacifique>
2023-09-25 11:56:26 +0200

    updt: parser behavior on array

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,34 +1,58 @@
-import { isJSON } from "valdie";
+import { isArray, isJSON } from "valdie";
 import { ParsedJSON } from "../interfaces/json.interface";
 import { InvalidJSON } from "../utils/errors";
 
-export default function parser(jsonString: string): ParsedJSON {
-  try {
-    const jsonData = JSON.parse(jsonString);
+interface ParsedObject {
+  keys: string[];
+  values: string[];
+  valueString: string;
+}
 
-    if (!isJSON(jsonData)) {
+const parseObject = (obj: Record<string, unknown>, parentKey?: string): ParsedObject => {
+  const keys: string[] = [];
+  const values: string[] = [];
+
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const currentKey = parentKey ? `${parentKey}.${key}` : key;
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        const { keys: subKeys, values: subValues } = parseObject(
+          obj[key] as Record<string, unknown>,
+          currentKey
+        );
+        keys.push(...subKeys);
+        values.push(...subValues);
+      } else {
+        keys.push(currentKey);
+        values.push(String(obj[key]));
+      }
+    }
+  }
+
+  const valueString = values.join("").replace(/\s/g, "");
+  return { keys, values, valueString };
+};
+
+export default function parser(jsonStrings: string[]): ParsedJSON[] {
+  try {
+    if (!isArray(jsonStrings)) {
+      throw new TypeError(`Expected an array of strings but received ${typeof jsonStrings}`);
+    }
+
+    const jsonObjects: Record<string, unknown>[] = jsonStrings.map((jsonString: string) => {
+      return JSON.parse(jsonString);
+    });
+
+    if (jsonObjects.some((obj) => !isJSON(obj))) {
       throw new InvalidJSON("Invalid JSON provided");
     }
 
-    const keys: string[] = [];
-    const values: string[] = [];
+    const parsedValuesArr: ParsedJSON[] = jsonObjects.map((obj: Record<string, unknown>) => {
+      const { keys, values, valueString } = parseObject(obj);
+      return { keys, values, valueString };
+    });
 
-    const parseObject = (obj: Record<string, unknown>) => {
-      for (const key in obj) {
-        if (typeof obj[key] === "object" && obj[key] !== null) {
-          keys.push(key);
-          parseObject(obj[key] as Record<string, unknown>);
-        } else {
-          keys.push(key);
-          values.push(String(obj[key]));
-        }
-      }
-    };
-
-    parseObject(jsonData);
-
-    const valueString = values.join("").replace(/\s/g, "");
-    return { keys, values, valueString };
+    return parsedValuesArr;
   } catch (error: any) {
     throw new InvalidJSON("Invalid JSON provided: " + error.message);
   }
