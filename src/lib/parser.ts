afd074f00cf5b9fb482ad5d3d6648d1a44e1a112
pacifiquem <Murangwa Pacifique>
2023-10-08 21:47:16 +0200

    feat: parsing but based on provided keys list

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -8,36 +8,9 @@
 import { isArray, isJSON } from "valdie";
 import { ParsedJSON } from "../interfaces/json.interface";
 import { InvalidJSON } from "../utils/errors";
+import { extractKeys, parseObject, indexer } from "../utils/helper";
 
-const indexer = (parsedStringArr: string[]): ParsedJSON[] => {
-  return parsedStringArr.map((parsedString, index) => ({
-    index,
-    value: parsedString,
-  }));
-};
-
-const parseObject = (obj: Record<string, unknown>, parentKey?: string): string => {
-  const values: string[] = [];
-
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const currentKey = parentKey ? `${parentKey}.${key}` : key;
-      if (typeof obj[key] === "object" && obj[key] !== null) {
-        const subValues = parseObject(obj[key] as Record<string, unknown>, currentKey);
-        values.push(...subValues);
-      } else {
-        values.push(String(obj[key]));
-      }
-    }
-  }
-
-  // Combine all values with their keys
-  const combinedValue = values.join("");
-
-  return combinedValue.replace(/\s/g, "").toLowerCase(); // remove any space and then return.
-};
-
-export default function parser(jsonStrings: string[]): ParsedJSON[] {
+export default function parser(jsonStrings: string[], keys: string[]): ParsedJSON[] {
   try {
     if (!isArray(jsonStrings)) {
       throw new TypeError(`Expected an array of strings but received ${typeof jsonStrings}`);
@@ -48,11 +21,13 @@ export default function parser(jsonStrings: string[]): ParsedJSON[] {
       return parsedString;
     });
 
-    if (jsonObjects.some((obj) => !isJSON(obj))) {
+    const desiredKeysObject: Record<string, unknown>[] = extractKeys(jsonObjects, keys);
+
+    if (desiredKeysObject.some((obj) => !isJSON(obj))) {
       throw new InvalidJSON("Invalid JSON provided");
     }
 
-    const parsedValuesArr: string[] = jsonObjects.map((obj: Record<string, unknown>) => {
+    const parsedValuesArr: string[] = desiredKeysObject.map((obj: Record<string, unknown>) => {
       return parseObject(obj);
     });
 
